d2f96c49de848f6c09a75d3de1738c6a1afa3d63
Malo Camaret <Malo Camaret>
2016-10-09 23:37:55 +0200

    At this point I don't even know

--- index.js
+++ index.js
@@ -5,60 +5,71 @@ const inquirer = require('inquirer')
 const db = require('sqlite')
 const fs = require('fs')
 
-db.open('default.db').then(()=>{
-     return db.run("CREATE TABLE IF NOT EXISTS searches (keyword, title, author, price, city, description, creationDate)");
-}).then(()=>{
-     return db.run("CREATE TABLE IF NOT EXISTS url (url VARCHAR, checked BOOLEAN)");
+db.open('default.db').then(() => {
+  return Promise.all([
+    db.run("CREATE TABLE IF NOT EXISTS searches (keyword, title, author, price, city, description, creationDate)"),
+    db.run("CREATE TABLE IF NOT EXISTS url (url VARCHAR, checked BOOLEAN)")
+  ])
+}).then(() => {
+  console.log('Database opened and schemas created')
+  start()
 })
 
-program
-  .version('1.0.0')
-  .option('-s, --search', 'Lance la recherche d\'annonces en ligne')
-  .option('-d, --database', 'Récupère les annonces enregistrées en base de données')
-  .option('-f, --file', 'Enregistre les données en base dans un fichier')
+function start() {
+  program
+    .version('1.0.0')
+    .option('-s, --search', 'Lance la recherche d\'annonces en ligne')
+    .option('-d, --database', 'Récupère les annonces enregistrées en base de données')
+    .option('-f, --file', 'Enregistre les données en base dans un fichier')
 
-program.parse(process.argv)
+  program.parse(process.argv)
 
-if (program.search) {
-  inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Entrez le mot clé à rechercher : ',
-      name: 'keyword'
-    }
-  ]).then((answers) => {
-    var url = 'https://www.leboncoin.fr/annonces/offres/aquitaine/?th=1&q=' + answers.keyword + '&parrot=0'
-    let keyword = answers.keyword
-
-    getAllUrls(url)
-    .then((res) => {
-      for (var i = 0, len = res.length; i < len; i++) {
-        insertUrl(res[i])
+  if (program.search) {
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: 'Entrez le mot clé à rechercher : ',
+        name: 'keyword'
       }
-    }).then(() => {
-      db.all("SELECT * FROM url WHERE checked = ?", false).then((res) => {
-        for (var i = 0, len = res.length; i < len; i++) {
-          getPageData(res[i].url, keyword)
+    ]).then((answers) => {
+      var url = 'https://www.leboncoin.fr/annonces/offres/aquitaine/?th=1&q=' + answers.keyword + '&parrot=0'
+      let keyword = answers.keyword
+
+      getAllUrls(url)
+      .then((res) => {
+        let i
+        for (i = res.length; i--;) {
+          insertUrl(res[i])
+        }
+        console.log('avant');
+        console.log(i);
+        if (i < 0) {
+          console.log('après');
+          db.all("SELECT * FROM url WHERE checked = ?", false).then((res) => {
+            for (var i = 0, len = res.length; i < len; i++) {
+              getPageData(res[i].url, keyword)
+            }
+          })
         }
       })
-    })
 
-  })
-} else if(program.database) {
-  console.log('--database');
-}  else if(program.file) {
-  console.log('--file');
-  // inquirer.prompt([
-  //   {
-  //     type: 'input',
-  //     message: 'Entrez le mot clé à rechercher : ',
-  //     name: 'keyword'
-  //   }
-  // ]).then((answers) => {
-  //
-  // })
-} else {
-  program.help()
+    })
+  } else if(program.database) {
+    console.log('--database');
+  }  else if(program.file) {
+    console.log('--file');
+    // inquirer.prompt([
+    //   {
+    //     type: 'input',
+    //     message: 'Entrez le mot clé à rechercher : ',
+    //     name: 'keyword'
+    //   }
+    // ]).then((answers) => {
+    //
+    // })
+  } else {
+    program.help()
+  }
 }
 
 function getAllUrls (url) {
@@ -86,20 +97,24 @@ function getPageData(i, keyword) {
       // .click('button.phoneNumber')
       // .wait('.phoneNumber a')
       .evaluate(function () {
-        let data = []
-        var title = document.querySelector('h1.no-border').innerText
-        var author = document.querySelector('.properties div.line_pro p a').innerText
-        var price = document.querySelector('.item_price span.value').innerText
-        var city = document.querySelector('div.line_city h2 span.value').innerText
-        var desc = document.querySelector('div.properties_description p.value').innerText
-        var createdAt = document.querySelector('p.line_pro').innerText
-        // var phone = document.querySelector('.phoneNumber a').innerText
-        data.push(title, author, price, city, desc, createdAt)
+        let price  = ''
+        if (document.querySelector('.item_price span.value') != null) {
+          price = document.querySelector('.item_price span.value').innerText
+        }
+        let data = {
+          title: document.querySelector('h1.no-border').innerText,
+          author: document.querySelector('.properties div.line_pro p a').innerText,
+          price: price,
+          city: document.querySelector('div.line_city h2 span.value').innerText,
+          desc: document.querySelector('div.properties_description p.value').innerText,
+          createdAt: document.querySelector('p.line_pro').innerText
+          // phone : document.querySelector('.phoneNumber a').innerText
+        }
         return data
       })
       .end()
       .then((data) => {
-        insertPageData(keyword, data[0], data[1], data[2], data[3], data[4], data[5])
+        insertPageData(keyword, data)
       })
       .then(() => {
         db.run("UPDATE url SET checked = ?", true);
@@ -110,19 +125,23 @@ function getPageData(i, keyword) {
       })
 }
 
-function insertPageData(keyword, title, author, price, city, desc, createdAt) {
-  db.run("INSERT INTO searches VALUES (?, ?, ?, ?, ?, ?, ?)", keyword, title, author, price, city, desc, createdAt);
-  console.log('db success (page data)');
+function insertPageData(keyword, data) {
+  db.run("INSERT INTO searches VALUES (?, ?, ?, ?, ?, ?, ?)", keyword, data.title, data.author, data.price, data.city, data.desc, data.createdAt)
+  .then(() => {
+    console.log('Insert successful (page data)');
+  })
 }
 
 function insertUrl(url) {
   db.all("SELECT COUNT(*) AS count FROM url WHERE url = ?", url)
   .then((res) => {
     if (res[0].count != 0) {
-      // console.log('match found');
+      console.log('Match found, no need to insert');
     } else {
-      db.run("INSERT INTO url VALUES (?, ?)", url, false);
-      // console.log('db success (url)');
+      db.run("INSERT INTO url VALUES (?, ?)", url, false)
+      .then(() => {
+        console.log('Insert successful (url)');
+      })
     }
   })
 }
